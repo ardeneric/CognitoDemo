7d0a1fb86160dc53fe3a79edaa7787f58d9f8d66
Eric Arden <earden@IM-21-B1-005.local>
2018-09-20 14:40:34 +0000

    user pool changed

--- src/main/java/com/cognito/config/CognitoConfig.java
+++ src/main/java/com/cognito/config/CognitoConfig.java
@@ -3,7 +3,6 @@ package com.cognito.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 
@@ -18,7 +17,6 @@ public class CognitoConfig extends WebSecurityConfigurerAdapter {
 		http
 		.csrf().disable()
 		.authorizeRequests()
-		.antMatchers("/oauth2/**").permitAll()
         .anyRequest().authenticated()
         .and()
         .httpBasic().disable();
